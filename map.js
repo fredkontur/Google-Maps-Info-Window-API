8718de5b8df6a2861d816455759d3d62fcb9e6f4
fredkontur <fredkontur@gmail.com>
2015-12-02 18:48:04 -0500

    Changed parameter for map center to be an address

--- map.js
+++ map.js
@@ -74,6 +74,6 @@ function createInfoBox(passedLoc, geocoder) {
 }
 
 if(ggMap_xyz_9753.loadMap && locs_xyz_9753[0]) {
-    var mapCenter = ggMap_xyz_9753.center || locs_xyz_9753[0];
-    google.maps.event.addDomListener(window, 'load', initMap(mapCenter.address, ggMap_xyz_9753.zoom, locs_xyz_9753));
+    var mapCenter = ggMap_xyz_9753.center || locs_xyz_9753[0].address;
+    google.maps.event.addDomListener(window, 'load', initMap(mapCenter, ggMap_xyz_9753.zoom, locs_xyz_9753));
 }
